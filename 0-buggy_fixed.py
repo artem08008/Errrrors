d413240092c09d50c25ecb2fad8b8a8c9ce72d64
artem08008 <94678638+artem08008@users.noreply.github.com>
2021-12-03 10:59:28 +0200

    Create 0-buggy_fixed.py

---
+++ 0-buggy_fixed.py
@@ -0,0 +1,27 @@
+first_num = 2;
+secord_num = 3;
+sum = firstNum + secondNum;
+print({ 'sum': sum  });
+
+# 1-fixed-something
+
+
+first_num = 2
+secord_num = 3
+sum = firstNum + secondNum
+print({ 'sum': sum  })
+
+# 2-improved 
+
+
+first_num = 2
+second_num = 3
+sum = firstNum + secondNum
+print({ 'sum': sum  })
+
+# 3-final
+
+first_num = 2
+second_num = 3
+sum = first_num + second_num 
+print({'sum': sum})

b2ac87ba612000bf408a0ab35fd799b4f5f9778c
artem08008 <94678638+artem08008@users.noreply.github.com>
2021-12-03 11:06:31 +0200

    Create 0-buggy_fixed.js

---
+++ 0-buggy_fixed.js
@@ -0,0 +1,25 @@
+let first_num = 2;
+let secord_num = 3;
+let sum = firstNum + secondNum;
+alert(sum);
+
+// 1-fixed-something 
+
+let first_num = 2;
+let second_num = 3;
+let sum = firstNum + secondNum;
+alert(sum);
+
+// 2-improved
+
+let first_num = 2;
+let second_num = 3;
+let sum = first_num + second_num;
+alert(sum);
+
+// 3-final
+
+let first_num = 2;
+let second_num = 3;
+let sum = first_num + second_num;
+conole.log(sum);

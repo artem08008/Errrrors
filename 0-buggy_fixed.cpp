b5ac66785ecb33a1bc315c0037aad2699be00621
artem08008 <94678638+artem08008@users.noreply.github.com>
2021-12-03 10:49:37 +0200

    Create 0-buggy_fixed.cpp

---
+++ 0-buggy_fixed.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+  int first_num = 2;
+  int secord_num = 3;
+  int sum = firstNum + secondNum;
+  printf("%d\n", sum);
+}
+
+// 1-fixed-something 
+
+#include <stdio.h>
+
+int main() {
+  int first_num = 2;
+  int second_num = 3;
+  int sum = firstNum + secondNum;
+  printf("%d\n", sum);
+}
+
+// 2-improved
+
+#include <stdio.h>
+
+int main() {
+  int first_num = 2;
+  int second_num = 3;
+  int sum = first_num + second_num;
+  printf("%d\n", sum);
+}
